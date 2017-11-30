c2b69d4d17fa8fe73804ddad86f011358cb1bf5d
Jonjo McKay <jonjo@jonjomckay.com>
2017-11-30 10:44:28 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+const Alexa = require("alexa-sdk");
+
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'TeamListing': function () {
+        const speechOutput = 'Your team members are Jonjo, Thameem, Yuval and Pazit';
+
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = 'This is the Hello World Sample Skill. ';
+        const reprompt = 'Say hello, to hear me speak.';
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak('Goodbye!');
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak('See you later!');
+        this.emit(':responseReady');
+    }
+};
